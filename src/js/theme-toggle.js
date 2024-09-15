e34537239ff309d53fb662205ae4deb2080738ad
Ethan <105263316+ethanokamura@users.noreply.github.com>
2024-09-14 21:25:47 -0700

    Update theme-toggle.js

--- src/js/theme-toggle.js
+++ src/js/theme-toggle.js
@@ -6,7 +6,7 @@ const screenshot02 = document.getElementById('screenshot-02');
 
 let isDarkMode = false;
 
-// On page load, set the correct icon visibility based on the theme
+// Initial theme setup on page load
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     themeToggleLightIcon.classList.remove('hidden');
     themeToggleDarkIcon.classList.add('hidden');
@@ -19,16 +19,14 @@ if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localS
     isDarkMode = false;
 }
 
-applyMode();
-
+// Button toggle event listener
 var themeToggleBtn = document.getElementById('theme-toggle');
-
 themeToggleBtn.addEventListener('click', function() {
-    // toggle icons inside button
+    // Toggle icon visibility
     themeToggleDarkIcon.classList.toggle('hidden');
     themeToggleLightIcon.classList.toggle('hidden');
 
-    // if set via local storage previously
+    // Handle theme toggling logic
     if (localStorage.getItem('color-theme')) {
         if (localStorage.getItem('color-theme') === 'light') {
             document.documentElement.classList.add('dark');
@@ -39,33 +37,28 @@ themeToggleBtn.addEventListener('click', function() {
             localStorage.setItem('color-theme', 'light');
             isDarkMode = false;
         }
-
-    // if NOT set via local storage previously
     } else {
         if (document.documentElement.classList.contains('dark')) {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
             isDarkMode = false;
-          } else {
+        } else {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
             isDarkMode = true;
         }
     }
-    applyMode();
+    applyMode(); // Update screenshots based on the theme
 });
 
-
 function applyMode() {
-  if (isDarkMode) {
-    // Apply dark mode
-    screenshot01.src = 'images/screenshot_01.png';
-    screenshot00.src = 'images/screenshot_00.png';
-    screenshot02.src = 'images/screenshot_02.png';
-  } else {
-    // Apply light mode
-    screenshot01.src = 'images/light_screenshot_01.png';
-    screenshot00.src = 'images/light_screenshot_00.png';
-    screenshot02.src = 'images/light_screenshot_02.png';
-  }
+    if (isDarkMode) {
+        screenshot01.src = 'images/screenshot_01.png';
+        screenshot00.src = 'images/screenshot_00.png';
+        screenshot02.src = 'images/screenshot_02.png';
+    } else {
+        screenshot01.src = 'images/light_screenshot_01.png';
+        screenshot00.src = 'images/light_screenshot_00.png';
+        screenshot02.src = 'images/light_screenshot_02.png';
+    }
 }
