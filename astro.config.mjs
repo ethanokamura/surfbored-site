22ce4829642954693cc4d0a98b77feaf177d7bb2
Ethan <ethanokamura3@gmail.com>
2024-10-06 20:25:39 -0700

    updated astro and fixed bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
 // import { loadEnv } from "vite";
 // const { BUILD_TYPE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
@@ -7,5 +8,5 @@ export default defineConfig({
   // site: 'ethanokamura.io',
   // base: '/', // 'ethanokamura/',
   output: 'static',
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
 });
